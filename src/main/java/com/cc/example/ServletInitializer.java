bbbda6bcdd960e113246981d5c73fc956d565b11
CalebChen <long8814@hotmail.com>
2016-07-20 12:05:51 +0800

    add Test and servlet

--- src/main/java/com/cc/example/ServletInitializer.java
+++ src/main/java/com/cc/example/ServletInitializer.java
@@ -7,6 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		System.out.println("123");
 		return application.sources(BootGradleSampleApplication.class);
 	}
 
---
+++ src/main/java/com/cc/example/Test.java
@@ -0,0 +1,5 @@
+package com.cc.example;
+
+public class Test {
+
+}
