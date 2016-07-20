66f618c91239be13718dd54abec3d15f5a18bc17
CalebChen <long8814@hotmail.com>
2016-07-20 11:48:55 +0800

    ㄏ

---
+++ src/main/java/com/cc/example/BootGradleSampleApplication.java
@@ -0,0 +1,12 @@
+package com.cc.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootGradleSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootGradleSampleApplication.class, args);
+	}
+}
